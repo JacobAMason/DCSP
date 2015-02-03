8f6beb882c862cacb7635e918bf286620d120029
JacobAMason <jm2232@msstate.edu>
2015-02-02 22:00:59 -0600

    ATEAM-1 #comment Init Commit

---
+++ desktop/src/com/DCSP/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.DCSP.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.DCSP.game.DCSPGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DCSPGame(), config);
+	}
+}
