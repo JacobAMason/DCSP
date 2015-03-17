e01f7b7c4252e18bfdc852ef58c86cb97cc46c98
aes421 <aes421@msstate.edu>
2015-03-16 22:50:06 -0500

    Starting Point for Web Dev
    I have no more details.

---
+++ AMAZEing_Game_Webdev/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+
+<html>
+    <head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    
+    <body>
+   
+            <div id = "Image">
+            </div>
+            <div id = "Title"> 
+                hi
+            </div>
+       
+    </body>
+</html>
+      
