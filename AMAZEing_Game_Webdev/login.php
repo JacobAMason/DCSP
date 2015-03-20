e1462116f53e7bb1fccb2421aa9e19212deb2db7
aes421 <aes421@msstate.edu>
2015-03-20 16:06:44 -0500

    Temp commit before merge
    Temp

--- AMAZEing_Game_Webdev/login.php
+++ AMAZEing_Game_Webdev/login.php
@@ -9,8 +9,9 @@
 
     
     <body>
-        <div id = "Container">
-            <div id = "Image"></div>
+    	<div id="container">
+            <img class="mazeImg" alt="mazeImg" src="http://pluto.cse.msstate.edu/~aes421/Maze.PNG"/>
+            
             <div id = "Title"> 
                 <center><font size="4" color = "white">
                 An <br>
@@ -18,8 +19,13 @@
                 Game!
                 </font></center>
             </div>
-        </div>
-       
+            
+            <form action="msstate.edu" method="POST" name = "login">
+            	username: <input type = "text" name = "user"/> &nbsp; &nbsp;
+            	password: <input type = "text" name = "pass"/>
+            	<input type = "submit" value = "Login"/>
+            </form>
+        </div>  
     </body>
 </html>
       
