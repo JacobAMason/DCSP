f6900a04c2b2572188ec7348f30133ce22e45e7e
aes421 <aes421@msstate.edu>
2015-04-14 11:58:53 -0500

    Changes to Submit an Idea
    Added a title for a cleaner look.  Also fixed a few small things in the
    menu.

--- WebDev/AMAZEing_Game_Webdev/SubmitAnIdea.php
+++ WebDev/AMAZEing_Game_Webdev/SubmitAnIdea.php
@@ -22,13 +22,11 @@ if(!isset($_SESSION["username"])) {
                             <a href="#"> Menu <span class="arrow">&#9660;</span></a>
  
                             <ul class="sub-menu">
-                                <?PHP if(isset($_SESSION["username"])) {?> <li><a href="loginSuccess.php">Home</a></li>
+                                <?PHP if(isset($_SESSION["username"])) {?> <li><a href="loginSuccess.php">Home</a></li> <li><a href="#">Account Info</a></li>
                                 <?PHP } else {?> <li><a href="index.html">Home</a></li> <?PHP }?>
-                                <li><a href="#">Account Info</a></li>
-                                <li><a href="HighScoresHTML.html">High Scores</a></li>
+                                <li><a href="HighScores.php">High Scores</a></li>
                                 <li><a href="#">Download Game</a></li>
                                 <li><a href="#">FAQ</a></li>
-                                <li><a href="SubmitAnIdea.php">Submit an idea</a></li>
                                 <li><a href="#">Donations :)</a></li>
                             </ul>
                         </li>
@@ -36,6 +34,14 @@ if(!isset($_SESSION["username"])) {
                 </nav>
             </div>
         </div>
+        
+        <div id="SubmitAnIdea">
+                <div id="words">
+                Submit An Idea
+               </div>
+       </div>
+        
+        
     <form class = "submit" action="submit.php" method="POST">
         <textarea rows="20" required>
         </textarea> 
