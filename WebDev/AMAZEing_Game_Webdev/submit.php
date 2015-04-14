d2b28d4f6bb907b78eef701aeca44c52eb4ddbf1
aes421 <aes421@msstate.edu>
2015-04-13 22:33:33 -0500

    Temp commit
    temp commit

---
+++ WebDev/AMAZEing_Game_Webdev/submit.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <style>
+            html, body {
+                height: 100%;
+                margin: 0;
+                padding: 0;
+                width: 100%;
+            }
+
+            html {
+                display: table;
+            }
+
+            body {
+                display: table-cell;
+            }
+        </style>
+    </head>
+    <body>
+        <div style="text-align:center; display: table-cell; vertical-align: middle;">
+            <a href="index.html" style="color:white;">Thank&nbsp;you&nbsp;for&nbsp;submitting&nbsp;your&nbsp;idea!</a>
+        </div>
+</html>
