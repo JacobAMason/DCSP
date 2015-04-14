a33c1976fd5317deb9832ea3159e46ac77149e91
aes421 <aes421@msstate.edu>
2015-04-13 21:42:41 -0500

    Submit and idea layout done (menu incorrect)
    HTML/CSS Layout is done.  No php has been written for the button to
    work.  Menu does not work.

--- WebDev/AMAZEing_Game_Webdev/SubmitAnIdea.php
+++ WebDev/AMAZEing_Game_Webdev/SubmitAnIdea.php
@@ -1,20 +1,43 @@
 <html>
  
 <head>
-    <link rel="stylesheet" type="text/css" href="GenericCSS.css">
+    <link rel="stylesheet" type="text/css" href="SubmitAnIdeaCSS.css">
 </head>   
     
 <body>
+    <div id="container">
     <a href="index.html">
-        <img class="logoImg" alt="logoImg" src="logoReg.png"/>
+         <div id="Title">
+             <img class="logoImg" alt="logoImg" src="logo.png"/>
+             <div class="menu-wrap">
+                <nav class="menu">
+                    <ul class="clearfix">
+                        <li>
+                            <a href="#"> Menu <span class="arrow">&#9660;</span></a>
+ 
+                            <ul class="sub-menu">
+                                <li><a href="#">Account Info</a></li>
+                                <li><a href="HighScoresHTML.html">High Scores</a></li>
+                                <li><a href="#">Download Game</a></li>
+                                <li><a href="#">FAQ</a></li>
+                                <li><a href="SubmitAnIdea.php">Submit an idea</a></li>
+                                <li><a href="#">Donations :)</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </a>
-    <form class = "login" action="submit.php" method="POST">
-        <textarea rows="10" cols="100" required>
+    <form class = "submit" action="submit.php" method="POST">
+        <textarea rows="20" required>
         </textarea> 
         <br/>
         <br/>
         <input type = "submit" value = "Submit idea"/>
     </form>
+        
+    </div>
 </body>
 
     
